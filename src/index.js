97c88daf7be9ef52c87f11dd9f1127705d6bc914
AlanSoto31 <soto.alan@hotmail.com>
2021-04-14 17:42:27 -0500

    Stablish comunication with the API

--- src/index.js
+++ src/index.js
@@ -3,4 +3,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 //import 'bootstrap';
 
 
-console.log("Hello");
+const getTemp = async (cityName) => {
+
+    let url = new URL('http://api.openweathermap.org/data/2.5/weather?q=London&appid=224b1af71ed4916e22748acda3c2d895');
+    url.searchParams.set('q', cityName); 
+    let new_url = url.toString();
+
+    try {
+        let result = await fetch(new_url);
+        let result2 = await result.json();
+        console.log(result2.main.feels_like);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getTemp("London");
+

747fa6e431a5b1d3b75681c9681faf4d41458fa7
AlanSoto31 <soto.alan@hotmail.com>
2021-04-15 14:40:25 -0500

    Add toggle degrees function

--- src/index.js
+++ src/index.js
@@ -1,37 +1,60 @@
 import './styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import 'bootstrap';
+import Store from "./storage";
 
-const getTemp = async (cityName) => {
 
-    let url = new URL('http://api.openweathermap.org/data/2.5/weather?q=&appid=224b1af71ed4916e22748acda3c2d895');
-    url.searchParams.set('q', cityName); 
+const getTemp = async (cityName, degrees) => {
+
+    let url = new URL('http://api.openweathermap.org/data/2.5/weather?q=&units=&appid=224b1af71ed4916e22748acda3c2d895');
+    url.searchParams.set('q', cityName);
+    url.searchParams.set('units', degrees); 
     let new_url = url.toString();
 
     try {
         let res = await fetch(new_url);
         let res2 = await res.json();
-        //console.log(result2.main.feels_like);
-        printInfo(res2); 
+        Store.storeInfo(res2.main);
+        printInfo();
     } catch (error) {
         console.log(error);
     }
 }
-/*
-const toggleDeg = async () => {
 
+
+
+const toggleDeg = () => {
+    let myObjStorage = JSON.parse(localStorage.getItem('myObjStorage'));
+    let degrees = document.getElementById("toggleDeg");    
+        if(degrees.textContent == "Fahrenheit"){
+            for (const property in myObjStorage) {
+                if(property != "humidity" && property != "pressure"){
+                    myObjStorage[property] = Math.round((myObjStorage[property] -32) * (5/9));
+                    degrees.innerText = "Celcius"
+                }
+            }
+        }else {
+            for (const property in myObjStorage) {
+                if(property != "humidity" && property != "pressure"){
+                    myObjStorage[property] = Math.round((myObjStorage[property] * (9/5))+32);
+                    degrees.innerText = "Fahrenheit"
+                }
+            }
+        }
+    Store.storeInfo(myObjStorage);
 }
-*/
-const printInfo = (obj) => {
+
+const printInfo = () => {
+    let myObjStorage = JSON.parse(localStorage.getItem('myObjStorage'));
     let body = document.getElementById("bodyTable");
     body.innerHTML = ``;
     body.innerHTML = `    
     <tr>
-    <th>${obj.main.temp}</th>
-    <td>${obj.main.feels_like}</td>
-    <td>${obj.main.temp_min}</td>
-    <td>${obj.main.temp_max}</td>
-    <td>${obj.main.humidity}</td>
+    <th>${Math.round(myObjStorage.temp)}</th>
+    <td>${Math.round(myObjStorage.feels_like)}</td>
+    <td>${Math.round(myObjStorage.temp_min)}</td>
+    <td>${Math.round(myObjStorage.temp_max)}</td>
+    <td>${Math.round(myObjStorage.humidity)}</td>
     </tr>
     `; 
 }
@@ -40,15 +63,14 @@ const printInfo = (obj) => {
 const takeCity = async (e) => {
     e.preventDefault();
     let city = document.getElementById("city");
-    //console.log(city.value);
-    getTemp(city.value);
+    getTemp(city.value, "standard");
 }
 
 // EVENTS
 
 document.getElementById("form").addEventListener("submit", takeCity);
 
-//document.getElementById("changeDeg").addEventListener("click", toggleDeg);
+document.getElementById("toggleDeg").addEventListener("click", toggleDeg);
 
 
 
